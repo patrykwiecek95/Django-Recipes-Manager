9b43ac29506930afda56dda0898d9e1bf8c2480b
patrykwiecek95 <patrykwiecek23@gmail.com>
2023-08-28 21:28:37 +0200

    added url configuration and view fucntion for home and recipe_detail

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.recipes_list, name='recipes_list'),
+    path('recipe/<int:pk>/', views.recipes_detail, name='recipe_detail'),
+    # path('', views.category, name='category'),
+    # path('', views.chef, name='chef'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Recipe
+from django.shortcuts import get_object_or_404
 
 
 def recipes_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipes/home.html', {'recipes': recipes})
+
+
+def recipes_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
