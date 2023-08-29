e0c7b311f186b68742a9e8af32a481ec6d0d20fe
patrykwiecek95 <patrykwiecek23@gmail.com>
2023-08-29 19:26:17 +0200

    added new  views and urls, added html template and static css

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.recipes_list, name='recipes_list'),
-    path('recipe/<int:pk>/', views.recipes_detail, name='recipe_detail'),
-    path('category/', views.category_page, name='category'),
-    # path('', views.chef, name='chef'),
+    path('', views.recipe_of_day, name='home'),
+    path('recipes/', views.recipes_list, name='recipes'),
+    path('recipes/<int:pk>/', views.recipes_detail, name='recipe_detail'),
+    path('categories/', views.categories_list, name='categories'),
+    path('categories/<int:pk>/', views.category_detail, name='category_detail'),
+    path('chefs/', views.chefs_page, name='chefs'),
+    path('chefs/<int:pk>/', views.chef_detail, name='chef_detail')
 ]
+
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from .models import Recipe, Category
+from .models import Recipe, Category, Chef
 from django.shortcuts import get_object_or_404
+import random
+
+
+def recipe_of_day(request):
+    recipes_data = Recipe.objects.all()
+    count = len(recipes_data)
+    random_index = random.randint(0, count - 1)
+    random_recipe = recipes_data[random_index]
+    return render(request, 'recipes/recipe_of_day.html', {'random_recipe': random_recipe, "index": random_index + 1})
 
 
 def recipes_list(request):
     recipes = Recipe.objects.all()
-    return render(request, 'recipes/home.html', {'recipes': recipes})
+    return render(request, 'recipes/recipes.html', {'recipes': recipes})
 
 
 def recipes_detail(request, pk):
@@ -13,6 +22,23 @@ def recipes_detail(request, pk):
     return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
 
 
-def category_page(request):
-    category = Category.objects.all()
-    return render(request, 'recipes/category.html', {'category': category})
+def categories_list(request):
+    categories = Category.objects.all()
+    return render(request, 'categories/categories.html', {'categories': categories})
+
+
+def category_detail(request, pk):
+    category_recipes = get_object_or_404(Category, pk=pk)
+    recipes = Recipe.objects.filter(categories=category_recipes)
+    return render(request, 'categories/category_detail.html', {'category': category_recipes, 'recipes': recipes})
+
+
+def chefs_page(request):
+    chefs = Chef.objects.all()
+    return render(request, 'chefs/chefs.html', {'chefs': chefs})
+
+
+def chef_detail(request, pk):
+    chef = get_object_or_404(Chef, pk=pk)
+    recipes = Recipe.objects.filter(chef=chef)
+    return render(request, 'chefs/chef_detail.html', {"chef": chef, "recipes": recipes})
